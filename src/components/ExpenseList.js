bc089f2a86c504206c94c07d20425ca888687262
hadidharmawan <hadidharmawan32@gmail.com>
2021-03-17 22:48:08 +0700

    add expense list component

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ExpenseList = () => {
+    const expenses = [
+        { id: 12, name: 'shopping', cost:40 },
+        { id: 13, name: 'holiday', cost:400 },
+        { id: 14, name: 'car service', cost: 50 },
+    ];
+
+    return (
+        <ul className='list-group'>
+            
+        </ul>
+    )
+}
+
+export default ExpenseList
