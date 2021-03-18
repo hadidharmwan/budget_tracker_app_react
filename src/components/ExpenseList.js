93feb256ba47a6916c883ae3b825214f159359a9
hadidharmawan <hadidharmawan32@gmail.com>
2021-03-18 23:27:02 +0700

    update { }to ()

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,13 +3,16 @@ import ExpenseItem from './ExpenseItem';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseList = () => {
-    const { expenses } = useContext(AppContext)
+   const { expenses } = useContext(AppContext);
 
     return (
-        <ul className='list-group'>
-            {expenses.map((expense) => {
-                <ExpenseItem id={expense.id} name={expense.name} cost={expense.cost} />
-            })}
+        <ul className='list-group mt-3 mb-3'>
+            {expenses.map((expense) => (
+                <ExpenseItem id={expense.id}
+                            name={expense.name} 
+                            cost={expense.cost} />                    
+            ))}
+            
         </ul>
     )
 }
